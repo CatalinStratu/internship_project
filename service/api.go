c417cf210dd8cbef552e150c7d162655a140ee16
Catalin Stratu <catalinstratu45@gmail.com>
2022-02-16 10:36:11 +0200

    Code refactoring request

--- service/api.go
+++ service/api.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -20,20 +21,42 @@ type Service interface {
 	Users()
 }
 
-func Users() []User {
+func Request() ([]byte, error) {
+	client := http.Client{}
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole", nil)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create request: %v", err)
+	}
+
+	rsp, err := client.Do(req)
+	if rsp != nil {
+		defer rsp.Body.Close()
+	}
+
+	if rsp.StatusCode == 404 || rsp.StatusCode == 403 || rsp.StatusCode == 401 || rsp.StatusCode == 500 {
+		return nil, fmt.Errorf("response failed with status code: %v", rsp.StatusCode)
+	}
+
+	body, err := io.ReadAll(rsp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read all response body: %v", err)
+	}
+	return body, nil
+}
+
+func Users() ([]User, error) {
 	var users []User
-	for i := 0; i < 1; i++ {
-		resp, err := http.Get("https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole")
-		if err != nil {
-			fmt.Println("No response from request")
-		}
+	for i := 0; i < 5; i++ {
 		var tempResult []User
-		body, err := io.ReadAll(resp.Body)                        // response body is []byte
-		if err := json.Unmarshal(body, &tempResult); err != nil { // Parse []byte to the go struct pointer
-			fmt.Println("Can not unmarshal JSON")
+		body, err := Request()
+		if err != nil {
+			return nil, fmt.Errorf("JSON ERROR: %v", err)
 		}
-		resp.Body.Close()
+		if err := json.Unmarshal(body, &tempResult); err != nil { // Parse []byte to the go struct pointer
+			return nil, fmt.Errorf("can not unmarshal JSON: %v", err)
+		}
+
 		users = append(users, tempResult...)
 	}
-	return users
+	return users, nil
 }
